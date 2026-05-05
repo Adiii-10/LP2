4dd4758b3cd83f726fe9917508aacc7bde9e7404
Adiii-10 <adiiityag1010@gmail.com>
2026-05-05 21:39:53 +0530

    Create selectionsort.py

---
+++ selectionsort.py
@@ -0,0 +1,23 @@
+def SelectionSort(arr):
+n = len(arr)
+for i in range(n - 1):
+min_ind = i
+for j in range(i + 1, n):
+if arr[j] < arr[min_ind]:
+min_ind = j
+arr[i], arr[min_ind] = arr[min_ind], arr[i]
+
+def print_array(arr):
+for val in arr:
+print(val, end=" ")
+print()
+
+if __name__ == "__main__":
+n = int(input("Enter the number of elements: "))
+arr = []
+print("Enter the elements:")
+for i in range(n):
+arr.append(int(input()))
+SelectionSort(arr)
+print("Sorted array:")
+print_array(arr)
